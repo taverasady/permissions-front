9a84d07ced1c30f68664afa5351d9746e1053d6d
Ady Taveras Courleaux <ady.taveras@solvex.com.do>
2020-12-10 13:31:48 -0400

    adding a test

--- tests/unit/home.spec.js
+++ tests/unit/home.spec.js
@@ -1,12 +1,26 @@
 import { shallowMount } from '@vue/test-utils'
+import moxios from 'moxios';
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import Home from '@/views/home.vue'
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 describe('Home View', () => {
-  it('view mounts properly', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(Home, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+
+  beforeEach(() => {
+    moxios.install();
+  });
+
+  afterEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+    moxios.uninstall();
+  });
+
+  it('Home renders without errors', () => {
+    const wrapper = shallowMount(Home, {});
+    expect(wrapper.vm).toBeTruthy();
   })
 })
